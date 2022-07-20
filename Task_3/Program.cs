225eab996179fc89c0ecbd228162e6bc2c53f344
Aleksey <busido_na87@mail.ru>
2022-07-20 11:50:04 +0300

    Task_3

---
+++ Task_3/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача№23:НаBходеЧисло N,наВыходеТаблицаКубовЧисел от 1 до N.
+Console.WriteLine("Задача№23:");
+Console.WriteLine("Введи число N:");
+double N = Convert.ToInt32(Console.ReadLine());
+int count = 1;
+if ( N < 1)
+{
+    Console.WriteLine("Неверно введено число");
+}
+else
+{
+    while(count <= N)
+    {
+    Console.WriteLine(Math.Pow(count,3));
+    count++;
+    }
+}
