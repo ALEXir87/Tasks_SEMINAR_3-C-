1f285b364cca58685a2c2747fed7d7a10e2e43e2
Aleksey <busido_na87@mail.ru>
2022-07-20 15:05:22 +0300

    Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,9 @@
+﻿// Задача №19: НаВходеПятизначноеЧисло,наВыходе - является ли число палиндромом.
+Console.WriteLine("Задача №19");
+Console.Write("Введи пятизначное число : ");
+string? number = Console.ReadLine();
+int [] array = new int[5];
+if (number[0] == number[4] && number[1] == number[3]){
+    Console.WriteLine("Это число палиндром");}
+else{
+    Console.WriteLine("Это число не палиндром");}
